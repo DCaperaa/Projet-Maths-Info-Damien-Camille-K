d8e85c46b03b8dc5ad8a32fb143cee1c3e602543
DCaperaa <damien.caperaa@mines-paristech.fr>
2019-10-20 18:27:39 +0200

    ajout simple_contour

--- Projet_maths_info.py
+++ Projet_maths_info.py
@@ -1,6 +1,6 @@
-def find_seed(f, c=0, eps=2**(-26)) :
+def find_seed(f, c=0, eps=2**(-26), x0=0) :
     def g(y) :
-        return f(0,y) - c
+        return f(x0,y) - c
     if g(0)*g(1) > 0 : #dans ce cas, notre hypothèse n'est pas vérifiée.
         return None
     else :
@@ -13,4 +13,16 @@ def find_seed(f, c=0, eps=2**(-26)) :
                 a = m
         return (a+b)/2 #Comme l'intervalle est de taille au plus 2*eps, on renvoit bien le 0 à + ou - eps.
 
+
+def simple_contour(f, c=0.0, delta=0.01) :
+    abscisses = []
+    ordonnées = []
+    X = 0
+    while X <= 1 :
+        t = find_seed(f, c, x0 = X)
+        if t != None :
+            abscisses.append(X)
+            ordonnées.append(t)
+        X = X + delta
+    return abscisses, ordonnées
 
