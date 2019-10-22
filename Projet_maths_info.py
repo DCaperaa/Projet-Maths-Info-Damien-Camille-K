e5ad7f56c330d63eba09b1d2d9c8cc44bfbd5b0e
DCaperaa <damien.caperaa@mines-paristech.fr>
2019-10-22 14:16:33 +0200

    dernières avancées

--- Projet_maths_info.py
+++ Projet_maths_info.py
@@ -14,7 +14,7 @@ def find_seed(f, c=0, eps=2**(-26), x0=0) :
         return (a+b)/2 #Comme l'intervalle est de taille au plus 2*eps, on renvoit bien le 0 à + ou - eps.
 
 
-def simple_contour(f, c=0.0, delta=0.01) :
+def simple_contour_scan_X(f, c=0.0, delta=0.01) :
     abscisses = []
     ordonnées = []
     X = 0
@@ -25,4 +25,40 @@ def simple_contour(f, c=0.0, delta=0.01) :
             ordonnées.append(t)
         X = X + delta
     return abscisses, ordonnées
+
+abscisses = []
+ordonnées = []
+def simple_contour_carre_elementaire_initiale(f, c=0, delta=0.01, x0, y0) :  #Permet juste de trouver une autre racine sur les bords du carré de centre notre racine initiale (x0, y0)
+    def find_seed_y0(f, c=0, eps=2**(-26), y0=0) :  #comme find_seed mais en faisant varier x à y0 fixé
+        def g(x) :
+            return f(x,y0) - c
+        if g(0)*g(1) > 0 :
+            return None
+        else :
+            a, b = 0, 1
+            while b-a >= 2*eps :
+                m=(a+b)/2
+                if g(m) >= 0 :
+                    b = m
+                else :
+                    a = m
+            return (a+b)/2
+    for x in [x0 + delta/2, x0 - delta/2] :
+        t = find_seed(f, c, x0 = x)
+        if t != None :
+            return x, t
+    for y in [y0 + delta/2, y0 - delta/2] :
+        t = find_seed_y0(f, c, x0 = x)
+        if t != None :
+            return t, y
+    return None
+
+def simple_contour_carre_elementaire(f, c=0, delta=0.01, x0, y0, numcôté) :
+
+
+
+
+def f(x,y) :
+    return x**2 + y**2 -0.5
+
 
